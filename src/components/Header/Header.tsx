ed3556f2fedd130e4e9b3ec0cbcde3a839e002ad
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-15 16:20:34 +0200

    Added custom fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '../../img/icons/Icons';
+import '../../fonts/fonts.css';
 
 function Header() {
   return (
@@ -6,6 +7,15 @@ function Header() {
       <a href="/">
         <Logo />
       </a>
+      <p style={{ fontFamily: 'BiroScriptPlus' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod{' '}
+      </p>
+      <p style={{ fontFamily: 'MessinaSansMono-Regular' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod{' '}
+      </p>
+      <p style={{ fontFamily: 'RightGrotesk-CompactBlack' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod{' '}
+      </p>
     </header>
   );
 }
