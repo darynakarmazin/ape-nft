ce56ebf361dabf44cb80a14a420074883934fe01
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-15 16:31:12 +0200

    Added styles for content-container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,11 @@ import Header from './Header/Header';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <main></main>
       <footer></footer>
-    </>
+    </div>
   );
 }
 
