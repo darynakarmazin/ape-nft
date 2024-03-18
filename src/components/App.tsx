e3d8b7f58834e1882f307e35a2a9caa2d781b4bb
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-18 14:44:19 +0200

    Created Mint section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import About from '../sections/About/About';
 import Arts from '../sections/Arts/Arts';
+import Mint from '../sections/ContactUs/ContactUs';
 import FAQ from '../sections/FAQ/FAQ';
 import Footer from '../sections/Footer/Footer';
 import Header from '../sections/Header/Header';
@@ -16,6 +17,7 @@ function App() {
         <MindMap />
         <FAQ />
         <Arts />
+        <Mint />
       </main>
       <Footer />
     </div>
---
+++ src/sections/ContactUs/ContactUs.tsx
@@ -0,0 +1,15 @@
+import { ContentContainer } from '../../components/ContentContainer/ContentContainer.styles';
+import { Section } from '../../components/Section/Section.styled';
+import { Title } from '../../components/Title/Title.styled';
+
+function Mint() {
+  return (
+    <Section id="mint">
+      <ContentContainer>
+        <Title>Are you in?</Title>
+      </ContentContainer>
+    </Section>
+  );
+}
+
+export default Mint;
